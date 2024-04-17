452ffc3e54b47252b7fccc8a52d17c3d6600977c
Stefano <snebo54@hotmail.com>
2024-04-17 11:26:03 +0100

    modified user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence:true
-  validates :password, presence:true, lengthe:{min: 5}
+  validates :password, presence:true, length:{minimum: 5}
 end
